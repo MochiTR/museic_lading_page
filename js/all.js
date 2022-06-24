9247f70a1d0bc0f6dec3ddbe328e007a211f2ce6
Mochi <mochitreets0304@gmail.com>
2022-06-24 19:50:29 +0800

    收錄歌手曲目區塊動畫

--- js/all.js
+++ js/all.js
@@ -1,1 +1,12 @@
 const cardList = document.querySelector('.cardList');
+console.log(cardList);
+gsap.defaults({
+    overwrite: true
+  });
+
+const clone = cardList.cloneNode(true);
+cardList.after(clone);
+
+const tl = gsap
+  .to('.cardList', { duration: 10, xPercent: -100, ease: "none", repeat: -1 })
+  .timeScale(1);
