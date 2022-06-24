0f640bd13ac67bd62bc713a192af6947236a5650
Mochi <mochitreets0304@gmail.com>
2022-06-24 20:21:56 +0800

    修正收錄歌手曲目區塊動畫RWD跑版問題

--- js/all.js
+++ js/all.js
@@ -1,12 +1,10 @@
 const cardList = document.querySelector('.cardList');
 console.log(cardList);
-gsap.defaults({
-    overwrite: true
-  });
+// gsap.defaults({
+//     overwrite: true
+//   });
 
 const clone = cardList.cloneNode(true);
 cardList.after(clone);
 
-const tl = gsap
-  .to('.cardList', { duration: 10, xPercent: -100, ease: "none", repeat: -1 })
-  .timeScale(1);
+gsap.to('.cardList', { duration: 10, xPercent: -100, ease: "none", repeat: -1 }).timeScale(1);
