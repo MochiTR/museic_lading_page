d0199b41e4040f02af2d47e8d21c9c50cf08f5d5
Mochi <mochitreets0304@gmail.com>
2022-07-16 10:51:26 +0800

    音樂播放功能

--- js/all.js
+++ js/all.js
@@ -9,35 +9,49 @@ cardList.after(clone);
 
 gsap.to('.cardList', { duration: 10, xPercent: -100, ease: "none", repeat: -1 }).timeScale(1);
 
-
 //album carousel
 var albums = document.getElementsByClassName('album')
 var transforms = [
   { x: 0,
     z: 0,
     scale: 1,
-    opacity: 1
+    opacity: 1,
+    title:"Got Me",
+    singer:"Nito-Onna, Poylow",
+    player:`<audio controls controlsList="nodownload"><source src="/audio/Poylow - Got Me (feat. Nito-Onna) [NCS Release].mp3" type="audio/mpeg">Your browser does not support the audio element.</audio>`
   },
   { x: '-100%',
     z: '-50px',
     scale: 0.8,
-    opacity: 0.5
+    opacity: 0.5,
+    title:"Sleeping Till Noon",
+    singer:"Andrew A, FROZT",
+    player:`<audio controls controlsList="nodownload"><source src="/audio/FROZT, Andrew A - Sleeping Till Noon (Feat.Moav) [NCS Release].mp3" type="audio/mpeg">Your browser does not support the audio element.</audio>`
   },
   { x: '-200%',
     z: '-75px',
     scale: 0.7,
-    opacity: 0.3
+    opacity: 0.3,
+    title:"Street Lights",
+    singer:"JJD, Azertion, Time To Talk",
+    player:`<audio controls controlsList="nodownload"><source src="/audio/Time To Talk, Azertion & JJD - Street Lights (Ft. Axollo) [NCS Release].mp3" type="audio/mpeg">Your browser does not support the audio element.</audio>`
   },
    { x: '200%',
     z: '-75px',
     scale: 0.7,
-    opacity: 0.3
+    opacity: 0.3,
+    title:"Throne (Lost Identities Remix)",
+    singer:"Lost Identites, Rival",
+    player:`<audio controls controlsList="nodownload"><source src="/audio/Rival - Throne (ft. Neoni) (Lost Identities Remix) [NCS Release].mp3" type="audio/mpeg">Your browser does not support the audio element.</audio>`
   },
   {
     x: '100%',
     z: '-50px',
     scale: 0.8,
-    opacity: 0.5
+    opacity: 0.5,
+    title:"Take Me",
+    singer:"Used, Andromedik",
+    player:`<audio controls controlsList="nodownload"><source src="/audio/Andromedik & Used - Take Me [NCS Release].mp3" type="audio/mpeg">Your browser does not support the audio element.</audio>`
   }
 ];
 
@@ -50,6 +64,10 @@ var nextTransform = function(x) {
   return x;
 };
 
+const songName = document.querySelector('#songTitle');
+const singerName = document.querySelector('#singerName');
+const musicPlayer = document.querySelector('#player');
+
 function next() {
   for (i = 0; i < albums.length; i++) {
     albums[i].style.transform = 
@@ -57,6 +75,9 @@ function next() {
       'translateZ(' + transforms[nextTransform(i)].z + ')' +
       'scale(' + transforms[nextTransform(i)].scale + ')';
     albums[i].style.opacity = transforms[nextTransform(i)].opacity;
+    songName.textContent = transforms[nextTransform(i)].title;
+    singerName.textContent = transforms[nextTransform(i)].singer;
+    musicPlayer.innerHTML = transforms[nextTransform(i)].player;
   }
   transforms.push(transforms.shift());
 }
